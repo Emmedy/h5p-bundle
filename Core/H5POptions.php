6099388af7de11a7c4727555b35e810d5da02710
Nieck Moorman <nieck@emmedy.com>
2018-03-22 15:22:16 +0100

    Some options will be stored and retrieved from database.

--- Core/H5POptions.php
+++ Core/H5POptions.php
@@ -3,66 +3,71 @@
 namespace Emmedy\H5PBundle\Core;
 
 
+use Doctrine\ORM\EntityManager;
+use Emmedy\H5PBundle\Entity\Option;
+
 class H5POptions
 {
     /**
      * @var array
      */
     private $config;
+    /**
+     * @var array
+     */
+    private $storedConfig = null;
 
     private $h5pPath;
     private $folderPath;
     private $kernelRootDir;
+    /**
+     * @var EntityManager
+     */
+    private $manager;
 
     /**
      * H5POptions constructor.
      * @param array $config
      * @param $kernelRootDir
+     * @param EntityManager $manager
      */
-    public function __construct(array $config, $kernelRootDir)
+    public function __construct(array $config, $kernelRootDir, EntityManager $manager)
     {
         $this->config = $config;
         $this->kernelRootDir = $kernelRootDir;
+        $this->manager = $manager;
     }
 
-    /**
-     * Get stored setting.
-     *
-     * @param string $name
-     *   Identifier for the setting
-     * @param string $default
-     *   Optional default value if settings is not set
-     * @return mixed
-     *   Whatever has been stored as the setting
-     */
-    public function getOption($name, $default = NULL) {
+    public function getOption($name, $default = null)
+    {
+        $this->retrieveStoredConfig();
+
+        if (isset($this->storedConfig[$name])) {
+            return $this->storedConfig[$name];
+        }
         if (isset($this->config[$name])) {
             return $this->config[$name];
         }
         return $default;
     }
 
-    /**
-     * Stores the given setting.
-     *
-     * @param string $name
-     *   Identifier for the setting
-     * @param mixed $value Data
-     *   Whatever we want to store as the setting
-     */
     public function setOption($name, $value)
     {
-        $this->config[$name] = $value;
+        $this->retrieveStoredConfig();
 
-        // Only update the setting if it has infact changed.
-//    if ($value !== \Drupal::config('h5p.settings')->get("h5p_{$name}")) {
-//      $config = \Drupal::configFactory()->getEditable('h5p.settings');
-//      $config->set("h5p_{$name}", $value);
-//      $config->save();
-//    }
+        if (!isset($this->storedConfig[$name]) || $this->storedConfig[$name] !== $value) {
+            $this->storedConfig[$name] = $value;
+            $option = $this->manager->getRepository('EmmedyH5PBundle:Option')->find($name);
+            if (!$option) {
+                $option = new Option($name);
+            }
+            $option->setValue($value);
+            $this->manager->persist($option);
+            $this->manager->flush();
+        }
     }
 
-    public function getUploadedH5pFolderPath($set = NULL)
+    public function getUploadedH5pFolderPath($set = null)
     {
         if (!empty($set)) {
             $this->folderPath = $set;
@@ -71,7 +76,7 @@ class H5POptions
         return $this->folderPath;
     }
 
-    public function getUploadedH5pPath($set = NULL)
+    public function getUploadedH5pPath($set = null)
     {
         if (!empty($set)) {
             $this->h5pPath = $set;
@@ -93,6 +98,17 @@ class H5POptions
     public function getLibraryFileUrl($libraryFolderName, $fileName)
     {
         return $this->getRelativeH5PPath() . "/libraries/$libraryFolderName/$fileName";
+    }
+
+    private function retrieveStoredConfig()
+    {
+        if ($this->storedConfig === null) {
+            $this->storedConfig = [];
+            $options = $this->manager->getRepository('EmmedyH5PBundle:Option')->findAll();
+            foreach ($options as $option) {
+                $this->storedConfig[$option->getName()] = $option->getValue();
+            }
+        }
     }
 
 }
