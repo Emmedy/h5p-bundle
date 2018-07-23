e0dc2903fe15425a415543a83924e87635624f94
Nieck Moorman <nieck@emmedy.nl>
2018-07-23 12:05:52 +0200

    Replace deprecated AssetHelper by Packages.

--- Core/H5PIntegration.php
+++ Core/H5PIntegration.php
@@ -5,7 +5,7 @@ namespace Emmedy\H5PBundle\Core;
 
 use Doctrine\ORM\EntityManager;
 use Emmedy\H5PBundle\Entity\Content;
-use Symfony\Bundle\FrameworkBundle\Templating\Helper\AssetsHelper;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
@@ -38,9 +38,9 @@ class H5PIntegration
      */
     private $requestStack;
     /**
-     * @var AssetsHelper
+     * @var Packages
      */
-    private $assetsHelper;
+    private $assetsPaths;
     /**
      * @var \H5PContentValidator
      */
@@ -54,10 +54,10 @@ class H5PIntegration
      * @param EntityManager $entityManager
      * @param RouterInterface $router
      * @param RequestStack $requestStack
-     * @param AssetsHelper $assetsHelper
+     * @param Packages $packages
      * @param \H5PContentValidator $contentValidator
      */
-    public function __construct(\H5PCore $core, H5POptions $options, TokenStorageInterface $tokenStorage, EntityManager $entityManager, RouterInterface $router, RequestStack $requestStack, AssetsHelper $assetsHelper, \H5PContentValidator $contentValidator)
+    public function __construct(\H5PCore $core, H5POptions $options, TokenStorageInterface $tokenStorage, EntityManager $entityManager, RouterInterface $router, RequestStack $requestStack, Packages $packages, \H5PContentValidator $contentValidator)
     {
         $this->core = $core;
         $this->options = $options;
@@ -65,7 +65,7 @@ class H5PIntegration
         $this->entityManager = $entityManager;
         $this->router = $router;
         $this->requestStack = $requestStack;
-        $this->assetsHelper = $assetsHelper;
+        $this->assetsPaths = $packages;
         $this->contentValidator = $contentValidator;
     }
 
@@ -317,6 +317,6 @@ class H5PIntegration
 
     private function getH5PAssetUrl()
     {
-        return $this->assetsHelper->getUrl($this->options->getH5PAssetPath());
+        return $this->assetsPaths->getUrl($this->options->getH5PAssetPath());
     }
 }
